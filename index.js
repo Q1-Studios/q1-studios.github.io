135d12e95f431d4a75e4744b25a32dcc2e168200
Elias Pozewaunig <elias.pozewaunig@aau.at>
2026-02-12 05:28:36 +0100

    Added imprint and privacy policy

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Logic for image slideshow
 let currentSlide = 0;
 showSlide(currentSlide);
 
@@ -17,4 +18,29 @@ function showSlide(index) {
 
 function advanceSlide(step) {
     showSlide(currentSlide + step);
+}
+
+// Logic for showing popups
+let popupContainer = document.getElementById("popup-container")
+popupContainer.addEventListener("click", function (e) {
+    if(e.target === this) {
+        closePopups()
+    }
+})
+
+function showImprint() {
+    popupContainer.classList.add("active");
+    document.getElementById("imprint").style.display = "block";
+}
+
+function showPrivacyPolicy() {
+    popupContainer.classList.add("active");
+    document.getElementById("privacy-policy").style.display = "block";
+}
+
+function closePopups() {
+    for(let child of popupContainer.children) {
+        child.style.display = "none";
+        popupContainer.classList.remove("active");
+    }
 }
